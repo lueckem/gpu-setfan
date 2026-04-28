8f90f8a8348ee8ea78fc07d159dd3743f608b13f
Marvin Lücke <31667561+lueckem@users.noreply.github.com>
2026-04-28 22:37:04 +0200

    feat: FanController

---
+++ src/fan_controller.rs
@@ -0,0 +1,80 @@
+use std::time::Instant;
+
+use tracing::debug;
+
+use crate::{fanspeed::FanSpeed, pi_controller::PIController, temperature::GPUTemperature};
+
+/// Fan controller using a PI controller.
+///
+/// The behavior is as follows:
+/// - The fans get turned on when temperature exceeds `fan_on_temperature`.
+/// - Then the fan speed is controlled with a PI controller aiming at `target_temperature`. The fan speed is always larger than the `min_fan_speed`.
+/// - If the temperature falls below the `fan_off_temperature`, the fans will get turned off again.
+pub struct FanController {
+    pi_controller: PIController,
+    target_temperature: GPUTemperature,
+    fan_on_temperature: GPUTemperature,
+    fan_off_temperature: GPUTemperature,
+    min_fan_speed: FanSpeed,
+    fans_off: bool,
+    last_eval: Instant,
+}
+
+impl FanController {
+    pub fn new(
+        target_temperature: GPUTemperature,
+        fan_on_temperature: GPUTemperature,
+        fan_off_temperature: GPUTemperature,
+        min_fan_speed: FanSpeed,
+        proportional_gain: f64,
+        integral_gain: f64,
+        smoothing_factor: f64,
+    ) -> Self {
+        Self {
+            pi_controller: PIController::new(proportional_gain, integral_gain, smoothing_factor),
+            target_temperature,
+            fan_on_temperature,
+            fan_off_temperature,
+            min_fan_speed,
+            fans_off: true,
+            last_eval: Instant::now(),
+        }
+    }
+
+    // TODO: Constructor that chooses proportional_gain, integral_gain, smoothing_factor automatically from target_temperature
+
+    pub fn eval(&mut self, temp: GPUTemperature) -> FanSpeed {
+        if self.fans_off && temp < self.fan_on_temperature {
+            return FanSpeed::zero();
+        }
+
+        if !self.fans_off && temp <= self.fan_off_temperature {
+            debug!("Fans turned off");
+            self.fans_off = true;
+            return FanSpeed::zero();
+        }
+
+        if self.fans_off {
+            debug!("Fans turned on");
+            self.fans_off = false;
+            self.last_eval = Instant::now();
+            self.pi_controller.reset();
+        }
+
+        let delta_t = self.last_eval.elapsed().as_secs_f64();
+        let err = temp.inner() - self.target_temperature.inner();
+        let u = self.pi_controller.update(err, delta_t);
+        self.last_eval = Instant::now();
+        self.convert_pi_to_fan_speed(u)
+    }
+
+    /// Maps value in [-1, 1] from PI controller to value in [u_0, 1],
+    /// where 0 <= u_0 < 1 is the minimum fan speed
+    fn convert_pi_to_fan_speed(&self, u: f64) -> FanSpeed {
+        let u0: f64 = self.min_fan_speed.into();
+        let u = u * (1.0 - u0) / 2.0 + (1.0 + u0) / 2.0;
+
+        // this never panics since the input was in [-1, 1]
+        u.try_into().unwrap()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,24 @@ use std::{thread::sleep, time::Duration};
 use nvml_wrapper::Nvml;
 use tracing::info;
 
-use crate::{interface::GPUInterface, nvidia::NvidiaGPU};
+use crate::{fan_controller::FanController, interface::GPUInterface, nvidia::NvidiaGPU};
 
+mod fan_controller;
 mod fanspeed;
 mod interface;
 mod logging;
 mod nvidia;
 mod pi_controller;
 mod temperature;
+
+const UPDATE_PERIOD: u64 = 1000; // in ms
+const TARGET_TEMPERATURE: f64 = 80.0;
+const FAN_ON_TEMPERATURE: f64 = 65.0;
+const FAN_OFF_TEMPERATURE: f64 = 55.0;
+const MIN_FAN_SPEED: u32 = 30;
+const PROPORTIONAL_GAIN: f64 = 0.1;
+const INTEGRAL_GAIN: f64 = 0.01;
+const SMOOTHING_FACTOR: f64 = 0.5;
 
 fn main() {
     logging::init_logging();
@@ -19,14 +29,23 @@ fn main() {
 
     let nvml = Nvml::init().unwrap();
     let device = nvml.device_by_index(0).unwrap();
-    let gpu = NvidiaGPU::init(device).unwrap();
+    let mut gpu = NvidiaGPU::init(device).unwrap();
     info!("Initialized Nvidia GPU '{}'", gpu.name);
+
+    let mut fan_controller = FanController::new(
+        TARGET_TEMPERATURE.try_into().unwrap(),
+        FAN_ON_TEMPERATURE.try_into().unwrap(),
+        FAN_OFF_TEMPERATURE.try_into().unwrap(),
+        MIN_FAN_SPEED.try_into().unwrap(),
+        PROPORTIONAL_GAIN,
+        INTEGRAL_GAIN,
+        SMOOTHING_FACTOR,
+    );
 
     loop {
         let temp = gpu.read_temperature().unwrap();
-        let temp: f64 = temp.into();
-        println!("{temp}");
-
-        sleep(Duration::from_millis(1000));
+        let target = fan_controller.eval(temp);
+        gpu.set_fan_speed(target).unwrap();
+        sleep(Duration::from_millis(UPDATE_PERIOD));
     }
 }
