b7b7b3e851cc4e759c921131803dbc1cc74fa06b
Marvin Lücke <31667561+lueckem@users.noreply.github.com>
2026-05-03 16:15:23 +0200

    feat: adapted release build logging to systemd

--- src/logging.rs
+++ src/logging.rs
@@ -2,19 +2,21 @@ use tracing_subscriber;
 
 // #[cfg(target_os = "linux")]
 pub fn init_logging() {
-    let max_level = if cfg!(debug_assertions) {
-        tracing::Level::DEBUG
+    if cfg!(debug_assertions) {
+        // Debug build
+        tracing_subscriber::fmt()
+            .with_writer(std::io::stdout)
+            .with_max_level(tracing::Level::DEBUG)
+            .init();
     } else {
-        tracing::Level::INFO
-    };
-
-    // TODO: disable timestamps in release build `.without_time()`
-    // since timestamps will already be added by journald
-
-    tracing_subscriber::fmt()
-        .with_writer(std::io::stdout)
-        .with_max_level(max_level)
-        .init();
+        // Release build
+        tracing_subscriber::fmt()
+            .with_writer(std::io::stdout)
+            .with_max_level(tracing::Level::INFO)
+            .with_target(false)
+            .without_time() // time is logged by systemd
+            .init();
+    }
 }
 
 // TODO: On windows, the program has to maintain the log file
