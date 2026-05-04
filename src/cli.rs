ff4737c8ca8b26724cc16792eae88471d5414c25
Marvin Lücke <31667561+lueckem@users.noreply.github.com>
2026-05-04 09:20:55 +0200

    feat: cli improved description

--- src/cli.rs
+++ src/cli.rs
@@ -1,22 +1,29 @@
 use clap::Parser;
 
-// TODO: description
+const DESCRIPTION: &str = "GPU fan control via temperature setpoint.
+
+Automatically adjusts fan speed to keep your GPU at the specified target temperature.
+The behavior is as follows:
+- At the start, the fans are off (speed = 0%).
+- The fans get turned on when the temperature exceeds the <FAN_ON> temperature.
+- Then the fan speed is controlled with a PI controller to maintain the [TARGET_TEMPERATURE]. The fan speed is always larger than <MIN_SPEED> to ensure stable operation.
+- When the temperature falls below the <FAN_OFF> temperature, the fans get turned off again.";
 
 #[derive(Parser, Debug)]
-#[command(version, about)]
+#[command(version, about=DESCRIPTION)]
 pub struct Cli {
     #[arg(default_value = "80", help = "in °C")]
     pub target_temperature: f64,
 
     #[arg(
         long,
-        help = "Temperature in °C at which fans turn on (must be < target)"
+        help = "Temperature in °C at which fans turn on; must be < target [default: target - 10]"
     )]
     pub fan_on: Option<f64>,
 
     #[arg(
         long,
-        help = "Temperature in °C at which fans turn off (must be < fan-on)"
+        help = "Temperature in °C at which fans turn off; must be < fan-on [default: fan-on - 5]"
     )]
     pub fan_off: Option<f64>,
 
